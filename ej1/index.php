2595dc83b7fe93a58df4ba3701d935404d95b52c
riku <cperez90@alumnes.politecnicllevant.cat>
2025-09-28 21:01:24 +0000

    condicional

--- ej1/index.php
+++ ej1/index.php
@@ -3,15 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php
+    $name = "Dark Matter";
+    $read = true;
+
+    if ($read){
+        $message = "You have read $name";
+    } else {
+        $message = "You have NOT read $name";
+    }
+    ?>
     <h1>
-        <?php
-        $saludo = "Hello";
-        echo $saludo.", World";
-        echo "<br>";
-        echo "$saludo Universe"
-        ?>
+        <?= $message ?>
     </h1>
 </body>
 </html>
