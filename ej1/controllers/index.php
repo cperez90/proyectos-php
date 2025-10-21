e454c479dc0f80932508b26b624f06962f004095
riku <cperez90@alumnes.politecnicllevant.cat>
2025-10-21 11:49:01 +0000

    PHP Sessions 101

--- ej1/controllers/index.php
+++ ej1/controllers/index.php
@@ -1,12 +1,6 @@
 <?php
 
-/*if($_SERVER['REQUEST_URI'] == '/ej1') {
-    echo 'bg-gray-900 text-white';
-} else {
-    echo 'text-gray-300';
-}*/
-
-//echo $_SERVER['REQUEST_URI'] == '/ej1' ? 'bg-gray-900 text-white' : 'text-gray-300';
+$_SESSION['name'] = 'Ale';
 
 view("index.view.php", [
     'heading' => 'Home'
