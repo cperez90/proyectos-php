286111a7426d05484b2b05a08c7482e83e38c3da
riku <cperez90@alumnes.politecnicllevant.cat>
2025-11-04 18:22:13 +0000

    Extract a Form Validation Object

--- ej1/Http/controllers/session/store.php
+++ ej1/Http/controllers/session/store.php
@@ -5,25 +5,19 @@
 use Core\Validator;
 use Core\App;
 use Core\Database;
+use Http\Forms\LoginForm;
 
 $db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$errors = [];
-if (!Validator::email($email)) {
-    $errors['email'] = 'Please provide a valid email address.';
-}
+$form = new LoginForm();
 
-if (!Validator::string($password)){
-    $errors['password'] = 'Please provide a valid password';
-}
-
-if (! empty($errors)) {
+if (! $form->validate($email, $password)) {
     view('session/create.view.php', [
-        'errors' => $errors
-    ]);
+        'errors' => $form->errors()
+        ]);
 }
 
 $user = $db->query('select * from user where  email = :email', [
