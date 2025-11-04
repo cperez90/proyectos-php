d80eba77d1cbb20e9c0581c28957d94a9df90ab3
riku <cperez90@alumnes.politecnicllevant.cat>
2025-11-04 18:44:16 +0000

    Extract an Authenticator Class

--- ej1/Core/functions.php
+++ ej1/Core/functions.php
@@ -38,22 +38,8 @@ function view($path,$attribute = [])
     require base_path('views/'.$path);
 }
 
-function login($user)
+function redirect($path)
 {
-    // mark that the user has logged in.
-    $_SESSION['user'] = [
-        'email' => $user['email']
-    ];
-
-    session_regenerate_id(true);
-}
-
-function logout()
-{
-    $_SESSION = [];
-    session_destroy();
-
-    $params = session_get_cookie_params();
-    setcookie('PHPSESSID','',time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
-
+    header("location: {$path}");
+    exit();
 }
--- ej1/Http/controllers/session/store.php
+++ ej1/Http/controllers/session/store.php
@@ -1,45 +1,25 @@
 <?php
 
-//log in the user if the credentials match.
-
-use Core\Validator;
-use Core\App;
-use Core\Database;
 use Http\Forms\LoginForm;
-
-$db = App::resolve(Database::class);
+use Core\Authenticator;
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
 
-if (! $form->validate($email, $password)) {
-    view('session/create.view.php', [
-        'errors' => $form->errors()
-        ]);
-}
+if ($form->validate($email, $password)) {
+    $auth = new Authenticator();
 
-$user = $db->query('select * from user where  email = :email', [
-    'email' => $email
-])->find();
+    if ($auth->attempt($email, $password)) {
 
-if ($user) {
-    if (password_verify($password, $user['password'])){
-        login([
-            'email' => $email
-        ]);
-
-        header('location: /');
-        exit();
+        redirect('/');
     }
+
+    $form->error('email','No matching account found that email address and password.');
+
 }
 
 view('session/create.view.php', [
-    'errors' => [
-        'email' => 'No matching account found that email address and password.'
-    ]
-    ]);
-
-
-
+    'errors' => $form->errors()
+]);
