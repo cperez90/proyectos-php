c7637536da7b92ea18b689a4e4b471faeb5e71a1
riku <cperez90@alumnes.politecnicllevant.cat>
2025-11-05 19:37:34 +0000

    reparar log out

--- ej1/Core/Authenticator.php
+++ ej1/Core/Authenticator.php
@@ -34,7 +34,11 @@ class Authenticator
 
     public function logout()
     {
-        Session::destroy();
+        $_SESSION = [];
+        session_destroy();
+
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 
     }
 }
